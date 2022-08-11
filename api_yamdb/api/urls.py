efcd47437999a5906dae530a41b1f12ee00a6725
Aleksandr Perepelkin <itperepelkin@gmail.com>
2022-08-11 18:45:55 +0300

    add router versions, add verbose names

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,11 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 from api.views import GenreViewSet, CategoryViewSet, TitleViewSet
 
-router = routers.DefaultRouter()
-router.register(r'genres', GenreViewSet, basename='Genre')
-router.register(r'categories', CategoryViewSet, basename='Category')
-router.register(r'titles', TitleViewSet, basename='Title')
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'genres', GenreViewSet, basename='Genre')
+router_v1.register(r'categories', CategoryViewSet, basename='Category')
+router_v1.register(r'titles', TitleViewSet, basename='Title')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,25 +5,33 @@ User = get_user_model()
 
 
 class Category(models.Model):
-    name = models.CharField("Название", max_length=256)
+    name = models.CharField(verbose_name="Название", max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Genre(models.Model):
-    name = models.CharField("Название", max_length=256)
+    name = models.CharField(verbose_name="Название", max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
 
 class Title(models.Model):
-    name = models.CharField("Название", max_length=256)
-    description = models.TextField("Описание", null=True, blank=True)
-    year = models.IntegerField('Год выпуска')
+    name = models.CharField(verbose_name="Название", max_length=256)
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
+    year = models.IntegerField(verbose_name="Год выпуска")
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL,
         db_column="category",
@@ -32,9 +40,13 @@ class Title(models.Model):
     )
     genre = models.ManyToManyField(
         Genre,
-        related_name='title',
-        verbose_name='Жанр'
+        related_name="title",
+        verbose_name="Жанр"
     )
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Произведение"
+        verbose_name_plural = "Произведения"
