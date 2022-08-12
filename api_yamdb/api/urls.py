1b51e4246aab5cf18e06ac616856098735774b98
CapitainFan <gudokrulit@mail.ru>
2022-08-12 17:23:56 +0300

    5 day : check slack and updated api.urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,7 @@
 from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                       ReviewViewSet, SignupAdminAPIView, SignupUserAPIView,
-                       TitleViewSet, UserViewAPI, UserViewSet)
+                       ReviewViewSet, SignupAdminAPIView,
+                       SignupUserAPIView, TokenAuthApiView,
+                       TitleViewSet, UserViewAPI, UserViewSet, )
 from django.urls import include, path
 from rest_framework import routers
 
@@ -22,7 +23,7 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/token/', SignupUserAPIView.as_view()),
+    path('v1/auth/token/', TokenAuthApiView.as_view()),
     path('v1/auth/signup/', SignupUserAPIView.as_view()),
     path('v1/users/', SignupAdminAPIView.as_view()),
     path('v1/users/<str:username>/', UserViewAPI.as_view()),
